6bd22893811f41ff48491684eed6dd19b3e25630
Kerim <kerm.accaunt@mail.ru>
2023-07-18 22:12:16 +0500

    #19 added auto save open api docs in file openApiDocs. Added guide for postman

---
+++ gazon-service/src/main/java/com/gitlab/config/AutoSaveOpenApiDocs.java
@@ -0,0 +1,30 @@
+package com.gitlab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import org.springframework.context.event.EventListener;
+
+import java.io.*;
+
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+
+@Configuration
+public class AutoSaveOpenApiDocs {
+
+    @EventListener
+    public void saveOpenApiDocs(ContextRefreshedEvent event) {
+        try (ReadableByteChannel readableByteChannel = Channels.newChannel(new URL("http://localhost:8080/v2/api-docs").openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("openApiDocs.json")) {
+
+            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
