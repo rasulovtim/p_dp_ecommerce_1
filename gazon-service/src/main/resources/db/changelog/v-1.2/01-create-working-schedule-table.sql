84f30f254669e6a9d048cbdb0c5a3c8029ce2468
Aleksandr Shibaev <sanbr2001@gmail.com>
2023-07-20 19:52:57 +0300

    update

--- gazon-service/src/main/resources/db/changelog/v-1.2/01-create-working-schedule-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE working_schedule (
-                                  id BIGSERIAL PRIMARY KEY,
-                                  day_of_week VARCHAR(10) NOT NULL,
-                                  from_time TIME NOT NULL,
-                                  to_time TIME NOT NULL
-);
--- gazon-service/src/main/resources/db/changelog/v-1.2/02-fill-working-schedule-table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO working_schedule (day_of_week, from_time, to_time)
-VALUES ('MONDAY', '08:00:00', '16:00:00');
-INSERT INTO working_schedule (day_of_week, from_time, to_time)
-VALUES ('TUESDAY', '09:00:00', '17:00:00');
-INSERT INTO working_schedule (day_of_week, from_time, to_time)
-VALUES ('WEDNESDAY', '10:00:00', '18:00:00');
