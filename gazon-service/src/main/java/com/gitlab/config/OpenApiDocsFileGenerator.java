16abf14f48931dfcfc2c972c9d6c7a6ce43f6229
Denis Pronin <de.pronin@gmail.com>
2023-07-21 14:18:33 +0400

    #19 minor guide_postman.md changes

--- gazon-service/src/main/java/com/gitlab/config/OpenApiDocsFileGenerator.java
+++ gazon-service/src/main/java/com/gitlab/config/OpenApiDocsFileGenerator.java
@@ -11,20 +11,17 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
-
 @Configuration
-public class AutoSaveOpenApiDocs {
+public class OpenApiDocsFileGenerator {
 
     @EventListener
-    public void saveOpenApiDocs(ContextRefreshedEvent event) {
-        try (ReadableByteChannel readableByteChannel = Channels.newChannel(new URL("http://localhost:8080/v2/api-docs").openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("openApiDocs.json")) {
-
+    public void generateOpenApiDocs(ContextRefreshedEvent event) {
+        try (ReadableByteChannel readableByteChannel = Channels.newChannel(
+                new URL("http://localhost:8080/v2/api-docs").openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("openApi.json")) {
             fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
