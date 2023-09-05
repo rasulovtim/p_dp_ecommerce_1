bb87ae5f8c4a2408080944ac0563f3b306db7240
ВашÐEGo87 <gokov87@vk.com>
2023-09-05 22:47:09 +0300

    ver1.0.2

--- gazon-service/src/main/java/com/gitlab/GazonMain.java
+++ gazon-service/src/main/java/com/gitlab/GazonMain.java
@@ -3,13 +3,9 @@ package com.gitlab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
-@RestController
-@EnableFeignClients
 public class GazonMain {
     public static void main(String[] args) {
         SpringApplication.run(GazonMain.class, args);
