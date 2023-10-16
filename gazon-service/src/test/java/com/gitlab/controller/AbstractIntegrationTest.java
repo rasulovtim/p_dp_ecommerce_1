d616dabaa18febadc3a55c210ee97719d2c454c6
Denis Pronin <de.pronin@gmail.com>
2023-10-16 13:16:46 +0600

    #92 updated pom's

--- gazon-service/src/test/java/com/gitlab/controller/AbstractIntegrationTest.java
+++ gazon-service/src/test/java/com/gitlab/controller/AbstractIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -18,7 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @ContextConfiguration(initializers = {
         PostgresSqlContainer.Initializer.class
 })
-@WithMockUser(roles = "ADMIN")
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
