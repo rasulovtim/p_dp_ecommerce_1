ca4ba996055b038dac962fc1b0a25197805f19f2
koolda205 <koolda@yandex.ru>
2023-10-20 23:42:33 +0300

    #13 additions: 3. add        @Repository        public interface PaymentRepository extends JpaRepository<Example, Long> {        }

---
+++ gazon-service/src/main/java/com/gitlab/controller/PaymentRestController.java
@@ -0,0 +1,87 @@
+package com.gitlab.controller;
+
+import com.gitlab.controllers.api.rest.ExampleRestApi;
+import com.gitlab.dto.ExampleDto;
+import com.gitlab.model.Example;
+import com.gitlab.service.ExampleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class PaymentRestController implements ExampleRestApi {
+
+    private final ExampleService exampleService;
+
+    @Override
+    public ResponseEntity<Page<ExampleDto>> getPage(Integer page, Integer size) {
+        if (page == null || size == null) {
+            return createUnPagedResponse();
+        }
+        if (page < 0 || size < 1) {
+            return ResponseEntity.noContent().build();
+        }
+
+        var examplePage = exampleService.getPage(page, size);
+        if (examplePage.getContent().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return createPagedResponse(examplePage);
+        }
+    }
+
+    private ResponseEntity<Page<ExampleDto>> createUnPagedResponse() {
+        var exampleDtos = exampleService.findAllDto();
+        if (exampleDtos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(new PageImpl<>(exampleDtos));
+    }
+
+    private ResponseEntity<Page<ExampleDto>> createPagedResponse(Page<Example> examplePage) {
+        var exampleDtoPage = exampleService.getPageDto(examplePage.getPageable().getPageNumber(), examplePage.getPageable().getPageSize());
+        return ResponseEntity.ok(exampleDtoPage);
+    }
+
+    @Override
+    public ResponseEntity<ExampleDto> get(Long id) {
+        return exampleService.findByIdDto(id)
+                .map(value -> ResponseEntity.ok(value))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @Override
+    public ResponseEntity<ExampleDto> create(ExampleDto exampleDto) {
+        ExampleDto savedExampleDto = exampleService.saveDto(exampleDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedExampleDto);
+    }
+
+    @Override
+    public ResponseEntity<ExampleDto> update(Long id, ExampleDto exampleDto) {
+        Optional<ExampleDto> updatedExampleDto = exampleService.updateDto(id, exampleDto);
+        return updatedExampleDto
+                .map(dto -> ResponseEntity.ok(dto))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        Optional<Example> example = exampleService.delete(id);
+        if (example.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok().build();
+        }
+    }
+}
---
+++ gazon-service/src/main/java/com/gitlab/service/PaymentService.java
@@ -0,0 +1,103 @@
+package com.gitlab.service;
+
+import com.gitlab.dto.ExampleDto;
+import com.gitlab.mapper.ExampleMapper;
+import com.gitlab.model.Example;
+import com.gitlab.repository.ExampleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final ExampleRepository exampleRepository;
+
+    private final ExampleMapper exampleMapper;
+
+    public List<Example> findAll() {
+        return exampleRepository.findAll();
+    }
+
+    public List<ExampleDto> findAllDto() {
+        List<Example> examples = exampleRepository.findAll();
+        return examples.stream()
+                .map(exampleMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public Page<Example> getPage(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return exampleRepository.findAll(pageRequest);
+    }
+
+    public Page<ExampleDto> getPageDto(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<Example> examplePage = exampleRepository.findAll(pageRequest);
+        return examplePage.map(exampleMapper::toDto);
+    }
+
+    public Optional<Example> findById(Long id) {
+        return exampleRepository.findById(id);
+    }
+
+    public Optional<ExampleDto> findByIdDto(Long id) {
+        return exampleRepository.findById(id)
+                .map(exampleMapper::toDto);
+    }
+
+    public Example save(Example example) {
+        return exampleRepository.save(example);
+    }
+
+    public ExampleDto saveDto(ExampleDto exampleDto) {
+        Example example = exampleMapper.toEntity(exampleDto);
+        Example savedExample = exampleRepository.save(example);
+        return exampleMapper.toDto(savedExample);
+    }
+
+    public Optional<Example> update(Long id, Example example) {
+        Optional<Example> optionalSavedExample = findById(id);
+        Example savedExample;
+        if (optionalSavedExample.isEmpty()) {
+            return optionalSavedExample;
+        } else {
+            savedExample = optionalSavedExample.get();
+        }
+        if (example.getExampleText() != null) {
+            savedExample.setExampleText(example.getExampleText());
+        }
+        return Optional.of(exampleRepository.save(savedExample));
+    }
+
+    public Optional<ExampleDto> updateDto(Long id, ExampleDto exampleDto) {
+        Optional<Example> optionalSavedExample = findById(id);
+        if (optionalSavedExample.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Example savedExample = optionalSavedExample.get();
+        if (exampleDto.getExampleText() != null) {
+            savedExample.setExampleText(exampleDto.getExampleText());
+        }
+
+        savedExample = exampleRepository.save(savedExample);
+        return Optional.of(exampleMapper.toDto(savedExample));
+    }
+
+    public Optional<Example> delete(Long id) {
+        Optional<Example> optionalSavedExample = findById(id);
+        if (optionalSavedExample.isEmpty()) {
+            return optionalSavedExample;
+        } else {
+            exampleRepository.deleteById(id);
+            return optionalSavedExample;
+        }
+    }
+}
