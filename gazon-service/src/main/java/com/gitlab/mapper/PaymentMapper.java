6d507bb5724a99d665c04410f47e1be594d7f41b
koolda205 <koolda@yandex.ru>
2023-10-21 00:30:30 +0300

    #13 additions: 5. add        @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)        public interface PaymentMapper {            PaymentDto toDto(Payment payment);            Payment toEntity(PaymentDto paymentDto);        }

--- gazon-service/src/main/java/com/gitlab/mapper/PaymentMapper.java
+++ gazon-service/src/main/java/com/gitlab/mapper/PaymentMapper.java
@@ -1,15 +1,12 @@
 package com.gitlab.mapper;
 
-import com.gitlab.dto.ExampleDto;
-import com.gitlab.model.Example;
+import com.gitlab.dto.PaymentDto;
 import com.gitlab.model.Payment;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PaymentMapper {
-
-    ExampleDto toDto(Payment payment);
-
-    Payment toEntity(ExampleDto exampleDto);
+    PaymentDto toDto(Payment payment);
+    Payment toEntity(PaymentDto paymentDto);
 }
