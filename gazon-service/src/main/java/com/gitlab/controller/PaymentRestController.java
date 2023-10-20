b397c9a806575617452a99aa2cc4a7378cd02ca2
koolda205 <koolda@yandex.ru>
2023-10-21 02:00:58 +0300

    #13 additions: 7. add        public class PaymentRestController implements PaymentRestApi

--- gazon-service/src/main/java/com/gitlab/controller/PaymentRestController.java
+++ gazon-service/src/main/java/com/gitlab/controller/PaymentRestController.java
@@ -1,21 +1,17 @@
 package com.gitlab.controller;
 
 import com.gitlab.controller.api.PaymentRestApi;
-import com.gitlab.controllers.api.rest.ExampleRestApi;
-import com.gitlab.dto.ExampleDto;
 import com.gitlab.dto.PaymentDto;
-import com.gitlab.model.Example;
 import com.gitlab.model.Payment;
 import com.gitlab.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -27,61 +23,41 @@ public class PaymentRestController implements PaymentRestApi {
     private final PaymentService paymentService;
 
     @Override
-    public ResponseEntity<Page<PaymentDto>> getPage(Integer page, Integer size) {
-        if (page == null || size == null) {
-            return createUnPagedResponse();
-        }
-        if (page < 0 || size < 1) {
-            return ResponseEntity.noContent().build();
-        }
-
-        var paymentPage = paymentService.getPage(page, size);
-        if (paymentPage.getContent().isEmpty()) {
+    public ResponseEntity<List<PaymentDto>> getAll() {
+        List<PaymentDto> payment = paymentService.findAllDto();
+        if (payment.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
-            return createPagedResponse(paymentPage);
+            return ResponseEntity.ok(payment);
         }
-    }
-
-    private ResponseEntity<Page<ExampleDto>> createUnPagedResponse() {
-        var exampleDtos = exampleService.findAllDto();
-        if (exampleDtos.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(new PageImpl<>(exampleDtos));
-    }
-
-    private ResponseEntity<Page<ExampleDto>> createPagedResponse(Page<Payment> examplePage) {
-        var exampleDtoPage = exampleService.getPageDto(examplePage.getPageable().getPageNumber(), examplePage.getPageable().getPageSize());
-        return ResponseEntity.ok(exampleDtoPage);
     }
 
     @Override
-    public ResponseEntity<ExampleDto> get(Long id) {
-        return exampleService.findByIdDto(id)
+    public ResponseEntity<PaymentDto> get(Long id) {
+        return paymentService.findByIdDto(id)
                 .map(value -> ResponseEntity.ok(value))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
-    public ResponseEntity<ExampleDto> create(ExampleDto exampleDto) {
-        ExampleDto savedExampleDto = exampleService.saveDto(exampleDto);
+    public ResponseEntity<PaymentDto> create(PaymentDto paymentDto) {
+        PaymentDto savedPaymentDto = paymentService.saveDto(paymentDto);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedExampleDto);
+                .body(savedPaymentDto);
     }
 
     @Override
-    public ResponseEntity<ExampleDto> update(Long id, ExampleDto exampleDto) {
-        Optional<ExampleDto> updatedExampleDto = exampleService.updateDto(id, exampleDto);
-        return updatedExampleDto
+    public ResponseEntity<PaymentDto> update(Long id, PaymentDto paymentDto) {
+        Optional<PaymentDto> updatePaymentDto = paymentService.updateDto(id, paymentDto);
+        return updatePaymentDto
                 .map(dto -> ResponseEntity.ok(dto))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
-    public ResponseEntity<Void> delete(Long id) {
-        Optional<Example> example = exampleService.delete(id);
-        if (example.isEmpty()) {
+    public ResponseEntity<PaymentDto> delete(Long id) {
+        Optional<Payment> payment = paymentService.delete(id);
+        if (payment.isEmpty()) {
             return ResponseEntity.notFound().build();
         } else {
             return ResponseEntity.ok().build();
