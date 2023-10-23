b8c31d3b8d931681223382f949602a4142fb6394
koolda205 <koolda@yandex.ru>
2023-10-24 00:02:49 +0300

    #13 additions: 18. 1. change class PaymentMapperTest extends AbstractIntegrationTest { 2. public abstract class PaymentMapper {
    it is work

--- gazon-service/src/main/java/com/gitlab/mapper/PaymentMapper.java
+++ gazon-service/src/main/java/com/gitlab/mapper/PaymentMapper.java
@@ -15,7 +15,6 @@ public abstract class PaymentMapper {
     protected OrderService orderService;
         @Mapping(source = "user.id", target = "userId")
         @Mapping(source = "bankCard", target = "bankCardDto")
-        @Mapping(source = "order", target = "orderId")
         public abstract PaymentDto toDto(Payment payment);
 
         public User mapUserIdToUser(Long userId) {
@@ -26,16 +25,8 @@ public abstract class PaymentMapper {
                     .orElseThrow(() -> new RuntimeException("User wasn't found"));
         }
 
-        public Order mapOrderIdToUser(Long orderId) {
-        if (orderId == null) {
-            return null;
-        }
-        return orderService.findById(orderId)
-                .orElseThrow(() -> new RuntimeException("Order wasn't found"));
-    }
         @Mapping(source = "userId", target = "user.id")
         @Mapping(source = "bankCardDto", target = "bankCard")
-        @Mapping(source = "orderId", target = "order.id")
         public abstract Payment toEntity(PaymentDto paymentDto);
 }
 
