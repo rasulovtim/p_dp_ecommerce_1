16edf857c956984a6f64f38b1b07df1c6f53d77c
koolda205 <koolda@yandex.ru>
2023-10-26 23:28:08 +0300

    #13 additions: 25. change back LQB it is work

--- gazon-service/src/main/resources/db/changelog/v-1.14/02-fill-order-table.sql
+++ gazon-service/src/main/resources/db/changelog/v-1.14/02-fill-order-table.sql
@@ -1,8 +1,2 @@
 INSERT INTO orders (shipping_address_id, shipping_date, order_code, create_date_time, sum, discount, bag_counter, user_id, order_status)
 VALUES (1, '2027-05-01', '123', '2027-05-01', 5, 5, '1', 1, 'NOT_PAID');
-INSERT INTO orders (shipping_address_id, shipping_date, order_code, create_date_time, sum, discount, bag_counter, user_id, order_status)
-VALUES (2, '2027-05-02', '122', '2027-05-02', 2, 2, '2', 2, 'NOT_PAID');
-INSERT INTO orders (shipping_address_id, shipping_date, order_code, create_date_time, sum, discount, bag_counter, user_id, order_status)
-VALUES (3, '2027-05-03', '133', '2027-05-03', 3, 3, '3', 3, 'PAID');
-
-
