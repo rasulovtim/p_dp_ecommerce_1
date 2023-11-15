983ea8f04730d3ade5373e415fb2932cd3484008
Denis Pronin <de.pronin@gmail.com>
2023-11-15 14:55:45 +0600

    #13 removed one row from 02-fill-payments-table.sql

--- gazon-service/src/main/resources/db/changelog/v-1.17/02-fill-payments-table.sql
+++ gazon-service/src/main/resources/db/changelog/v-1.17/02-fill-payments-table.sql
@@ -4,8 +4,6 @@ INSERT INTO payments (bank_card_id, payment_status, create_date_time, order_id, 
 VALUES (2, 'PAID', '2023-02-22 12:02', 1, 2500, 2);
 INSERT INTO payments (bank_card_id, payment_status, create_date_time, order_id, sum, user_id)
 VALUES (3, 'OVERDUE', '2023-03-23 13:03', 1, 3500, 3);
-INSERT INTO payments (bank_card_id, payment_status, create_date_time, order_id, sum, user_id)
-VALUES (4, 'CANCELED', '2023-04-24 14:04', 1, 4500, 4);
 
 
 
