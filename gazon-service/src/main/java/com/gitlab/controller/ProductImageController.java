ed32821840465a2a586c58df26992ab5564028a2
ttokaev <toktim1709@bk.ru>
2023-12-25 16:32:44 +0300

    add productImage frond and fix searchView

--- gazon-service/src/main/java/com/gitlab/controller/ProductImageController.java
+++ gazon-service/src/main/java/com/gitlab/controller/ProductImageController.java
@@ -2,11 +2,14 @@ package com.gitlab.controller;
 
 import com.gitlab.controllers.api.rest.ProductImageRestApi;
 import com.gitlab.dto.ProductImageDto;
+import com.gitlab.dto.ReviewImageDto;
 import com.gitlab.mapper.ProductImageMapper;
 import com.gitlab.model.ProductImage;
 import com.gitlab.service.ProductImageService;
 import com.gitlab.util.ImageUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -25,14 +29,33 @@ public class ProductImageController implements ProductImageRestApi {
     private final ProductImageMapper productImageMapper;
 
     @Override
-    public ResponseEntity<long[]> getAll() {
-        var productImages = productImageService.findAll();
-        return productImages.isEmpty() ?
-                ResponseEntity.noContent().build() :
-                ResponseEntity.ok(productImages.stream()
-                        .map(ProductImage::getId).mapToLong(Long::valueOf).toArray());
+    public ResponseEntity<Page<ProductImageDto>> getPage(Integer page, Integer size) {
+        if (page == null || size == null) {
+            return createUnPagedResponse();
+        }
+        if (page < 0 || size < 1) {
+            return ResponseEntity.noContent().build();
+        }
+        var productImagePage = productImageService.getPage(page, size);
+        if (productImagePage.getContent().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return createPagedResponse(productImagePage);
+        }
+    }
+    private ResponseEntity<Page<ProductImageDto>> createPagedResponse(Page<ProductImage> productImagePage) {
+        var productImageDtoPage = productImageService.getPageDto(productImagePage.getPageable().getPageNumber(),
+                productImagePage.getPageable().getPageSize());
+        return ResponseEntity.ok(productImageDtoPage);
     }
 
+    private ResponseEntity<Page<ProductImageDto>> createUnPagedResponse() {
+        var productImageDtos = productImageService.findAllDto();
+        if (productImageDtos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(new PageImpl<>(productImageDtos));
+    }
 
 
     @Override
@@ -53,6 +76,14 @@ public class ProductImageController implements ProductImageRestApi {
     }
 
     @Override
+    public ResponseEntity<List<ProductImageDto>> getAllByProduct(Long id) {
+        List<ProductImageDto> productImageDtos = productImageService.findAllByProductIdDto(id);
+        return productImageDtos.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok(productImageDtos);
+    }
+
+    @Override
     public ResponseEntity<ProductImageDto> create(ProductImageDto productImageDto) {
         ProductImageDto saveProductImageDto = productImageService.saveDto(productImageDto);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -60,20 +91,11 @@ public class ProductImageController implements ProductImageRestApi {
     }
 
     @Override
-    public ResponseEntity<ProductImageDto> update(MultipartFile file, @PathVariable Long id) throws IOException {
-        Optional<ProductImage> productImage = productImageService.findById(id);
-
-        if (productImage.isEmpty())
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-
-        var imageToBeUpdated = new ProductImage();
-        imageToBeUpdated.setName(file.getOriginalFilename());
-        imageToBeUpdated.setData(ImageUtils.compressImage(file.getBytes()));
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ImageUtils.decompressAndReturnDto(productImageMapper
-                        .toDto(productImageService
-                                .update(id, imageToBeUpdated).orElse(null))));
+    public ResponseEntity<ProductImageDto> update(Long id, ProductImageDto productImageDto) {
+        Optional<ProductImageDto> updatedProductImageDto = productImageService.updateDto(id, productImageDto);
+        return updatedProductImageDto
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
--- gazon-service/src/main/java/com/gitlab/repository/ProductImageRepository.java
+++ gazon-service/src/main/java/com/gitlab/repository/ProductImageRepository.java
@@ -2,10 +2,15 @@ package com.gitlab.repository;
 
 import com.gitlab.model.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
+    @Query("SELECT r FROM ProductImage r WHERE r.someProduct.id = :id and r.someProduct.entityStatus = 'ACTIVE'")
+    List<ProductImage> findAllBySomeProductId(Long id);
 }
