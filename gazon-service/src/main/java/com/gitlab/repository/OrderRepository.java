8c870884590a8a4999e32f59e717e8a8d58b6bf8
Vad-77 <vadim.d-163@yandex.ru>
2024-01-27 12:40:11 +0400

    changes OrderRepository

--- gazon-service/src/main/java/com/gitlab/repository/OrderRepository.java
+++ gazon-service/src/main/java/com/gitlab/repository/OrderRepository.java
@@ -1,9 +1,32 @@
 package com.gitlab.repository;
 
 import com.gitlab.model.Order;
+import lombok.NonNull;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Override
+    @NonNull
+    Optional<Order> findById(@NonNull Long id);
+
+
+    @Override
+    @NonNull
+    @Query("SELECT r FROM Order r WHERE r.entityStatus = 'ACTIVE' order by r.id asc")
+    Page<Order> findAll(Pageable pageable);
+
+    @Override
+    @NonNull
+    @Query("SELECT r FROM Order r WHERE r.entityStatus = 'ACTIVE' order by r.id asc")
+    List<Order> findAll();
+
 }
